5797609fa24def09c0627e1d37266a566dbcda2b
Junker <dk-junker@ya.ru>
2017-05-27 22:45:31 +0700

    main commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ionic-multirange
 Ionic v1 multi range component
+
+##Usage
+```html
+	<link href="lib/ionic-multirange/ionic-multirange.css" rel="stylesheet">
+	<script src="lib/ionic-multirange/ionic-multirange.js"></script>
+
+	...
+
+	<li class="item range range-positive">
+		<ui-multi-range 
+			ng-step="1" 
+			ng-min="18" 
+			ng-max="90" 
+			ng-model-min="data.age_min" 
+			ng-model-max="data.age_max" 
+			ng-change-min="changed()" 
+			ng-change-max="changed()">
+		</ui-multi-range>
+	</li>
+```
+
+```javascript
+	angular.module('aimdate', ['ionic', 'ionic-multiselect']);
+
+```
---
+++ ionic-multirange.js
@@ -0,0 +1,73 @@
+(function (ng) {
+"use strict";
+
+    var module = ng.module('ionic-multiselect', []);
+
+    module.directive('uiMultiRange', ['$compile', function ($compile) {
+        var directive = {
+            restrict: 'E',
+            scope: {
+                ngModelMin: '=',
+                ngModelMax: '=',
+                ngMin: '=',
+                ngMax: '=',
+                ngStep: '=',
+                ngChangeMin: '&',
+                ngChangeMax: '&'
+            },
+            link: link
+        };
+
+        return directive;
+
+        function link ($scope, $element, $attrs) {
+            var min, max, step, $inputMin = angular.element('<input type="range">'), $inputMax;
+            $scope.ngChangeMin = $scope.ngChangeMin || angular.noop;
+            $scope.ngChangeMax = $scope.ngChangeMax || angular.noop;
+
+            if (typeof $scope.ngMin == 'undefined') {
+                min = 0;
+            } else {
+                min = $scope.ngMin;
+                $inputMin.attr('min', min);
+            }
+            if (typeof $scope.ngMax == 'undefined') {
+                max = 0;
+            } else {
+                max = $scope.ngMax;
+                $inputMin.attr('max', max);
+            }
+            if (typeof $scope.ngStep == 'undefined') {
+                step = 0;
+            } else {
+                step = $scope.ngStep;
+                $inputMin.attr('step', step);
+            }
+            $inputMax = $inputMin.clone();
+            $inputMin.attr('ng-model', 'ngModelMin');
+            $inputMax.attr('ng-model', 'ngModelMax');
+            $compile($inputMin)($scope);
+            $compile($inputMax)($scope);
+            $element.append($inputMin).append($inputMax);
+            $scope.ngModelMin = $scope.ngModelMin || min;
+            $scope.ngModelMax = $scope.ngModelMax || max;
+
+            $scope.$watch('ngModelMin', function (newVal, oldVal) {
+                if (newVal > $scope.ngModelMax) {
+                    $scope.ngModelMin = oldVal;
+                } else {
+                    $scope.ngChangeMin();
+                }
+            });
+
+            $scope.$watch('ngModelMax', function (newVal, oldVal) {
+                if (newVal < $scope.ngModelMin) {
+                    $scope.ngModelMax = oldVal;
+                } else {
+                    $scope.ngChangeMax();
+                }
+            });
+        }
+    }]);
+
+})(angular);
